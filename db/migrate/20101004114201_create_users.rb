05a7ea26157b483f32dfab41fdd397767b97ac4c
Tomáš Holas <tomas.holas@gmail.com>
2010-10-04 13:43:21 +0200

    scaffold

---
+++ db/migrate/20101004114201_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.date :born
+      t.text :note
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
